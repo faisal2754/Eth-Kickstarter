d13c91d985c1c144944db0e4cb491793f699848f
Faisal <saleemfs27@gmail.com>
2021-07-15 21:14:11 +0200

    Begin next routing setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const next = require('next')
+const app = next({ dev: process.env.NODE_ENV !== 'production' })
+const routes = require('./routes')
+const handler = routes.getRequestHandler(app)
+
+const { createServer } = require('http')
+app.prepare().then(() => {
+   createServer(handler).listen(3000, (err) => {
+      if (err) throw err
+      console.log('Listening on localhost:3000')
+   })
+})
