27ef235d9250c62dcc0b1ddaec39bfdb0951e873
Faisal <saleemfs27@gmail.com>
2021-07-15 21:04:10 +0200

    Set up create campaign form

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import 'semantic-ui-css/semantic.min.css'
 import { Card, Button } from 'semantic-ui-react'
 import factory from '../ethereum/factory'
 import Layout from '../components/Layout'
