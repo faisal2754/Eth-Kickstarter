216cb2c9132e41ce6d8bc20458cd7b2183faef73
Faisal <saleemfs27@gmail.com>
2021-07-17 23:17:49 +0200

    Completed request form and requests display

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -9,7 +9,9 @@ class NewRequest extends Component {
    state = {
       value: '',
       description: '',
-      recipient: ''
+      recipient: '',
+      errorMessage: '',
+      loading: false
    }
 
    static async getInitialProps(props) {
@@ -17,11 +19,35 @@ class NewRequest extends Component {
       return { address }
    }
 
+   onSubmit = async (event) => {
+      event.preventDefault()
+
+      const campaign = Campaign(this.props.address)
+      const { description, value, recipient } = this.state
+
+      this.setState({ loading: true, errorMessage: '' })
+
+      try {
+         const accounts = await web3.eth.getAccounts()
+         await campaign.methods
+            .createRequest(description, web3.utils.toWei(value, 'ether'), recipient)
+            .send({ from: accounts[0] })
+         Router.pushRoute(`/campaigns/${this.props.address}/requests`)
+      } catch (err) {
+         this.setState({ errorMessage: err.message })
+      }
+
+      this.setState({ loading: false })
+   }
+
    render() {
       return (
          <Layout>
+            <Link route={`/campaigns/${this.props.address}/requests`}>
+               <a>Back</a>
+            </Link>
             <h3>Create a Request</h3>
-            <Form>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                <Form.Field>
                   <label>Description</label>
                   <Input
@@ -48,7 +74,15 @@ class NewRequest extends Component {
                   />
                </Form.Field>
 
-               <Button primary>Create</Button>
+               <Message
+                  error
+                  header="Something went wrong :("
+                  content={this.state.errorMessage}
+               />
+
+               <Button primary loading={this.state.loading}>
+                  Create
+               </Button>
             </Form>
          </Layout>
       )
